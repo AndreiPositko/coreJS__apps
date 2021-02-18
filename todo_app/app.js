9fb6834442111717f9f32cff1252c55f25e06f63
Andrey <andrei.positko.gb@gmail.com>
2021-02-18 14:46:40 +0200

    put info into local storage

--- todo_app/app.js
+++ todo_app/app.js
@@ -6,6 +6,8 @@ const taskInput = document.querySelector('#task');
 
 //Load all event listeners
 const loadEventListeners = () => {
+  // DOM load even
+  document.addEventListener('DOMContentLoaded', getTasks);
   //add task event
   form.addEventListener('submit', addTask);
   //remove task event
@@ -16,30 +18,86 @@ const loadEventListeners = () => {
   filter.addEventListener('keyup', filterTasks);
 };
 
+// Get Tasks from LS
+const getTasks = () => {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach((task) => {
+    // Create element li
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    //Put text into variable
+    liText = document.createTextNode(task);
+    // Append text to li
+    li.appendChild(liText);
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon HTML
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+
+    //Append li to ul
+    taskList.appendChild(li);
+  });
+};
+
+// Add tasks
 const addTask = (e) => {
   if (taskInput.value === '') {
     alert('Add a task');
   }
-
+  // Create element li
   const li = document.createElement('li');
+  // Add class
   li.className = 'collection-item';
-
+  //Put text into variable
   liText = document.createTextNode(taskInput.value);
+  // Append text to li
   li.appendChild(liText);
-
+  // Create new link element
   const link = document.createElement('a');
+  // Add class
   link.className = 'delete-item secondary-content';
-
+  // Add icon HTML
   link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
   li.appendChild(link);
 
+  //Append li to ul
   taskList.appendChild(li);
 
+  // Store in LS
+  storeTaskInLocalStorage(taskInput.value);
+
+  //Clear input
   taskInput.value = '';
 
   console.log(li);
 
   e.preventDefault();
+};
+
+// Store Task
+const storeTaskInLocalStorage = (task) => {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 };
 
 //Remove Task
